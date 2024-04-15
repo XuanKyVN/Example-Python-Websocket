371637c20262dbf5d910cec871e195dd8fd3f87d
XUAN KY AUTOMATION <97943446+XuanKyVN@users.noreply.github.com>
2024-04-15 17:11:38 +0700

    Add files via upload

---
+++ WebSocket/App1.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from websockets.exceptions import ConnectionClosed
+from fastapi.templating import Jinja2Templates
+import uvicorn    #  WEBSOCKET
+import asyncio
+import cv2
+from ultralytics import YOLO
+
+app = FastAPI()
+camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.websocket("/ws")
+async def get_stream(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            success, frame = camera.read()
+
+            if not success:
+                break
+            else:
+
+                model = YOLO(r"C:\Users\Admin\PythonLession\YoloModel\yolov8n.pt")
+                result = model.predict(frame)
+
+                frame = result[0].plot()
+                cv2.rectangle(frame,(10,5),(40,300),(255,0,0),2)
+
+                ret, buffer = cv2.imencode('.jpg', frame)
+                await websocket.send_text("WEB CAM PHAM XUAN KY")
+                await websocket.send_bytes(buffer.tobytes())
+            await asyncio.sleep(0.03)
+    except (WebSocketDisconnect, ConnectionClosed):
+        print("Client disconnected")
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost', port=8000)
+    #uvicorn.run(app, host='192.168.1.12', port=8000)
